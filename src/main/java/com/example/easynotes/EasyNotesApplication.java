dd35757f87afdfdf6fb72fa055958397bb75d8b5
nietzky <jszulc@verizon.net>
2020-02-02 16:08:21 -0500

    first commit

---
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -0,0 +1,26 @@
+package com.example.easynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class EasyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotesApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/notes").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+}
